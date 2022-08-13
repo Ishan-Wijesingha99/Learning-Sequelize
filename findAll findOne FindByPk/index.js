ef8edd79e4d2daa19004970ece29869f5dc3a203
Ishan <ishanwijes@gmail.com>
2022-08-13 23:07:30 +1000

    findByPk() done

--- findAll findOne FindByPk/index.js
+++ findAll findOne FindByPk/index.js
@@ -68,23 +68,54 @@ Book.sync({force: true})
     ]) 
 })
 .then(async () => {
-    // findAll() mirrors the SELECT statement in MySQL
+    // findAll() mirrors the SELECT * statement in MySQL
     // Book.findAll() returns a promise, so you need to wait until it's done fetching all the rows, and then only log it to the console
     // the result will be an array of objects, one for each row
-    const arrayOfAllRows = await Book.findAll()
-    console.log(arrayOfAllRows)
+    const arrayOfAllRows = await Book.findAll().then(data => {
+        // what is returned by Book.findAll() is an array, so you need to loop through that array and convert each element using toJSON() and then log that
+        data.forEach(element => console.log(element.toJSON()))
+    })
 
-    // if we only want some columns, we can pass in an object into findAll()
-    const array2 = await Book.findAll({attributes: ['title', 'author']})
-    console.log(array2)
+
+
+    // if we only want some columns, we can pass an object into findAll()
+    const array2 = await Book.findAll({attributes: ['title', 'author']}).then(data => {
+        // what is returned by Book.findAll() is an array, so you need to loop through that array and convert each element using toJSON() and then log that
+        data.forEach(element => console.log(element.toJSON()))
+    })
+
+
 
     // we can even return these values with another name
     // pass in nested arrays where first element is what you're getting out of the table and second element is what you want to name it
-    const array3 = await Book.findAll({attributes: [['title', 'bookName'], ['author', 'authorName']]})
-    console.log(array3)
+    const array3 = await Book.findAll({attributes: [['title', 'bookName'], ['author', 'authorName']]}).then(data => {
+        // what is returned by Book.findAll() is an array, so you need to loop through that array and convert each element using toJSON() and then log that
+        data.forEach(element => console.log(element.toJSON()))
+    })
+
 
     // if we want to fetch all the column values except some of them, we can use exclude: []
-    const array4 = await Book.findAll({attributes: {exclude: ['title']}})
-    console.log(array4)
+    const array4 = await Book.findAll({attributes: {exclude: ['title']}}).then(data => {
+        // what is returned by Book.findAll() is an array, so you need to loop through that array and convert each element using toJSON() and then log that
+        data.forEach(element => console.log(element.toJSON()))
+    })
+
+
+
+
+    // findByPk
+    // this returns a promise as well so we should wait for it
+    // because we called findByPk on Book, sequelize knows what table we're dealing with
+    // then we specify the value of the primary key, and what will be returned is the row corresponding to that primary key (the model instance)
+    const object1 = await Book.findByPk(2).then(data => data.toJSON())
+    console.log(object1)
+
+
+
+    // findOne()
+
+
+
+    
 })
 .catch(err => console.log(err))
